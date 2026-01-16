272143ede516df451eadd26b0064849ad13825b3
GavinArdhijaya91 <gavin.ardhijaya@gmail.com>
2026-01-16 10:28:39 +0700

    Add files via upload

---
+++ backend/src/data/users.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: 1,
+    email: "gavin.ardhijaya@gmail.com",
+    password: "QWERTY987654321",
+    role: "admin"
+  },
+  {
+    id: 2,
+    email: "user@gmail.com",
+    password: "user123",
+    role: "customer"
+  }
+];
